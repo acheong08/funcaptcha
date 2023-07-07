26b3cf30c10b02000a4df657b08665b6d19a8f21
Antonio <acheong@student.dalat.org>
2023-07-07 11:27:39 +0800

    brute force

--- cmd/main.go
+++ cmd/main.go
@@ -1,49 +1,40 @@
 package main
 
 import (
-	"fmt"
 	"log"
 
 	"github.com/acheong08/funcaptcha"
 )
 
 func main() {
-	token, hex, err := funcaptcha.GetOpenAIToken()
-	log.Println(token)
+	for {
+		token, hex, err := funcaptcha.GetOpenAIToken()
+		log.Println(token)
 
-	if err == nil {
-		return
-	}
-	log.Printf("error getting token: %v\n", err)
-	// Start a challenge
-	session, err := funcaptcha.StartChallenge(token, hex)
-	if err != nil {
-		log.Fatalf("error starting challenge: %v\n", err)
-	}
-	log.Println("Challenge started!")
+		if err == nil {
+			return
+		}
+		log.Printf("error getting token: %v\n", err)
+		// Start a challenge
+		session, err := funcaptcha.StartChallenge(token, hex)
+		if err != nil {
+			log.Fatalf("error starting challenge: %v\n", err)
+		}
+		log.Println("Challenge started!")
 
-	err = session.RequestChallenge(false)
-	if err != nil {
-		log.Fatalf("error requesting challenge: %v\n", err)
-	}
-	log.Println(session.ConciseChallenge)
-	log.Println("Downloading challenge")
-	_, err = funcaptcha.DownloadChallenge(session.ConciseChallenge.URLs, false)
-	if err != nil {
-		log.Fatalf("error downloading challenge: %v\n", err)
-	}
-	log.Println("Challenge downloaded!")
-	// User input here
-	fmt.Println("Please enter the index of the image based on the following instructions:")
-	fmt.Println(session.ConciseChallenge.Instructions)
-	var index int
-	_, err = fmt.Scanln(&index)
-	if err != nil {
-		log.Fatalf("error reading input: %v\n", err)
-	}
-	log.Println(index)
-	err = session.SubmitAnswer(index)
-	if err != nil {
-		log.Fatalf("error submitting answer: %v\n", err)
+		err = session.RequestChallenge(false)
+		if err != nil {
+			log.Fatalf("error requesting challenge: %v\n", err)
+		}
+		log.Println(session.ConciseChallenge)
+
+		err = session.SubmitAnswer(2)
+		if err != nil {
+			log.Printf("error submitting answer: %v\n", err)
+			continue
+		} else {
+			log.Println("Answer submitted!")
+			break
+		}
 	}
 }
